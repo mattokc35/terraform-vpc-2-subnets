fe5b9ff00891625c604e6ea1dea341bf84380ce1
Matthew Chen <mattokc35@ibm.com>
2022-10-12 11:35:13 -0500

    add vsi

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "ibm_is_vpc" "testacc_vpc" {
 }
 
 resource "ibm_is_vpc_routing_table" "testacc_vpc" {
-  name = "example-routing-table"
+  name = "testacc-vpc-routing-table"
   vpc  =  ibm_is_vpc.testacc_vpc.id
 
 
@@ -50,6 +50,8 @@ resource "ibm_is_subnet" "testacc_vpc" {
 }
 
 
+//public gateway
+
 resource "ibm_is_public_gateway" "testacc_vpc" {
   name = "testacc-vpc-public-gateway"
   vpc  = ibm_is_vpc.testacc_vpc.id
@@ -59,4 +61,46 @@ resource "ibm_is_public_gateway" "testacc_vpc" {
 resource "ibm_is_subnet_public_gateway_attachment" "testacc_vpc" {
   subnet                = ibm_is_subnet.testacc_vpc.id
   public_gateway         = ibm_is_public_gateway.testacc_vpc.id
+}
+
+//VSI
+
+resource "ibm_is_ssh_key" "testacc_vpc" {
+  name       = "testacc-vpc-ssh"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR"
+}
+
+resource "ibm_is_instance" "testacc_vpc" {
+  name    = "testacc-vpc-instance"
+  image   = "r006-d806e4b3-ac71-4274-b4e2-8efc5bb75393"
+  profile = "bx2-2x8"
+  metadata_service_enabled  = false
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.testacc_vpc.id
+    primary_ipv4_address = "10.0.1.6"
+    allow_ip_spoofing = true
+  }
+
+  network_interfaces {
+    name   = "eth1"
+    subnet = ibm_is_subnet.testacc_vpc.id
+    allow_ip_spoofing = false
+  }
+
+  vpc  = ibm_is_vpc.testacc_vpc.id
+  zone = "${var.ibm_region}-1"
+  keys = [ibm_is_ssh_key.testacc_vpc.id]
+
+  //User can configure timeouts
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
+// primary_ipv4_address deprecation 
+output "primary_ipv4_address" {
+  # value = ibm_is_instance.testacc_vpc.primary_network_interface.0.primary_ipv4_address // will be deprecated in future
+  value = ibm_is_instance.testacc_vpc.primary_network_interface.0.primary_ip.0.address // use this instead 
 }
